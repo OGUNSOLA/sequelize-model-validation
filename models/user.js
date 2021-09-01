8f4961b3cf76aa5be869d85a71b89431e1ca6354
OLUBUKOLA OGUNSOLA <olubukola@me.com>
2021-09-01 17:20:39 -0400

    added contraints

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = (sequelize) => {
   class User extends Model {}
@@ -18,8 +19,11 @@ module.exports = (sequelize) => {
     },
     email: {
       type: DataTypes.STRING,
-            allowNull: false,
-            validate:{
+      allowNull: false,
+      unique:{
+        msg: "The email you enetered already exist"
+      },
+      validate:{
        notNull:{
          msg:"An email is required",
        },
@@ -37,14 +41,14 @@ module.exports = (sequelize) => {
        notNull:{
          msg:"Birthday is required",
        },
-       notEmpty:{
-         msg: "Please provide a birth date",
+       isDate:{
+         msg: "Please provide a valid birth date",
        }
       }
 
     },
     password: {
-      type: DataTypes.STRING,
+      type: DataTypes.VIRTUAL,
             allowNull: false,
              validate:{
        notNull:{
@@ -52,11 +56,30 @@ module.exports = (sequelize) => {
        },
         notEmpty:{
          msg: "Please provide a password",
+       },
+       len:{
+         args:[8,20],
+         msg: "password should be between 8 and 20 characters"
        }
       }
 
+    },
+    confirmPassword:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      set(val){
+    if(val === this.password){
+    const hashedPassword = bcrypt.hashSync(val, 10);
+    this.setDataValue('confirmPassword', hashedPassword);
     }
-  }, { sequelize });
+          },
+          validate:{
+            notNull:{
+              msg: "Password must match"
+            }
+          }
+        }
+      }, { sequelize });
 
-  return User;
+      return User;
 };
